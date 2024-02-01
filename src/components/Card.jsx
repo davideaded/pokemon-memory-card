e0b702e3a29bbf1a27c01c5ba4c437e70c2fffc3
davideaded <david.castrodc033@gmail.com>
2024-02-01 11:08:13 -0300

    Fixed function call on component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,8 @@
 import '../styles/card.css'
 
-export default function Card({title, image}) {
-	const handleClick = () => {
-		console.log('clicked');
-	}
-
+export default function Card({title, image, handleCardClick}) {
 	return (
-		<div className="cardInfo" onClick={() => handleClick()}>
+		<div className="cardInfo" onClick={handleCardClick}>
 			<img src={image} />
 			<h1>{title}</h1>
 		</div>
